0031f5f9ee43215e793940c58750db43837cedd0
JP CodeCraft <augustoayub85@gmail.com>
2024-04-03 16:08:29 -0300

    settings

--- AlbionDataAvalonia/Network/Models/AlbionServer.cs
+++ AlbionDataAvalonia/Network/Models/AlbionServer.cs
@@ -4,14 +4,14 @@ public class AlbionServer
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string IpBase { get; set; }
+    public string HostIp { get; set; }
     public string UploadUrl { get; set; }
 
-    public AlbionServer(int id, string name, string ipBase, string uploadUrl)
+    public AlbionServer(int id, string name, string hostIp, string uploadUrl)
     {
         Id = id;
         Name = name;
-        IpBase = ipBase;
+        HostIp = hostIp;
         UploadUrl = uploadUrl;
     }
 
--- AlbionDataAvalonia/Settings/AppSettings.cs
+++ AlbionDataAvalonia/Settings/AppSettings.cs
@@ -1,6 +1,11 @@
-﻿namespace AlbionDataAvalonia.Settings;
+﻿using AlbionDataAvalonia.Network.Models;
+using System.Collections.Generic;
+
+namespace AlbionDataAvalonia.Settings;
 
 public class AppSettings
 {
     public string? NPCapDownloadUrl { get; set; }
+    public string? PacketFilterPortText { get; set; }
+    public List<AlbionServer> AlbionServers { get; set; } = new List<AlbionServer>();
 }
