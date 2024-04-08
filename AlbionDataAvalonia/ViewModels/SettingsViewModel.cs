07cc89cdddd30a3805a7863f4db0d1688afbf3f9
JP Codecraft <jpcode@craft.com>
2024-04-07 22:29:12 -0300

    Show Pow Solve Time average

--- AlbionDataAvalonia/ViewModels/SettingsViewModel.cs
+++ AlbionDataAvalonia/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AlbionDataAvalonia.Settings;
+using AlbionDataAvalonia.State;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AlbionDataAvalonia.ViewModels;
@@ -6,18 +7,25 @@ namespace AlbionDataAvalonia.ViewModels;
 public partial class SettingsViewModel : ViewModelBase
 {
     private readonly SettingsManager _settingsManager;
+    private readonly PlayerState _playerState;
 
     [ObservableProperty]
     private UserSettings userSettings;
+
+    [ObservableProperty]
+    private double powSolveTimeAverage;
 
     public SettingsViewModel()
     {
     }
 
-    public SettingsViewModel(SettingsManager settingsManager)
+    public SettingsViewModel(SettingsManager settingsManager, PlayerState playerState)
     {
         _settingsManager = settingsManager;
+        _playerState = playerState;
 
         userSettings = _settingsManager.UserSettings;
+
+        _playerState.OnPlayerStateChanged += (sender, args) => PowSolveTimeAverage = _playerState.PowSolveTimeAverage;
     }
 }
