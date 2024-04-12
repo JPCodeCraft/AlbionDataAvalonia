f4fd6729464c8447619f9e6d29c5b7bea0473f3f
JP Codecraft <jpcode@craft.com>
2024-04-12 20:33:39 -0300

    Error handling in listener servie.

--- AlbionDataAvalonia/Network/Services/NetworkListenerService.cs
+++ AlbionDataAvalonia/Network/Services/NetworkListenerService.cs
@@ -68,18 +68,25 @@ namespace AlbionDataAvalonia.Network.Services
             {
                 new Thread(() =>
                 {
-                    Log.Debug("Open... {Device}", device.Description);
-
-                    device.OnPacketArrival += new PacketArrivalEventHandler(PacketHandler);
-                    device.Open(new DeviceConfiguration
+                    try
                     {
-                        Mode = DeviceModes.MaxResponsiveness,
-                        ReadTimeout = 5000
-                    });
-                    var ips = _settingsManager.AppSettings.AlbionServers.Select(s => $"host {s.HostIp}");
-                    var filter = $"({string.Join(" or ", ips)}) and {_settingsManager.AppSettings.PacketFilterPortText}";
-                    device.Filter = filter;
-                    device.StartCapture();
+                        Log.Debug("Open... {Device}", device.Description);
+
+                        device.OnPacketArrival += new PacketArrivalEventHandler(PacketHandler);
+                        device.Open(new DeviceConfiguration
+                        {
+                            Mode = DeviceModes.MaxResponsiveness,
+                            ReadTimeout = 5000
+                        });
+                        var ips = _settingsManager.AppSettings.AlbionServers.Select(s => $"host {s.HostIp}");
+                        var filter = $"({string.Join(" or ", ips)}) and {_settingsManager.AppSettings.PacketFilterPortText}";
+                        device.Filter = filter;
+                        device.StartCapture();
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Debug("Error initializing device {Device}: {Message}", device.Name, ex.Message);
+                    }
                 })
                 .Start();
             }
@@ -111,9 +118,16 @@ namespace AlbionDataAvalonia.Network.Services
                             {
                                 Task.Run(() =>
                                 {
-                                    device.StopCapture();
-                                    device.Close();
-                                    Log.Debug("Close... {Device}", device.Description);
+                                    try
+                                    {
+                                        device.StopCapture();
+                                        device.Close();
+                                        Log.Debug("Close... {Device}", device.Description);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Log.Debug("Error closing device {Device}: {Message}", device.Name, ex.Message);
+                                    }
                                 });
                             }
                         }
