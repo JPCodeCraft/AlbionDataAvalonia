14b70c84b1e727be358f39b090e5fa548c35180f
JP Codecraft <jpcode@craft.com>
2024-04-19 22:47:49 -0300

    Catchinig and uploading loadout prices from the quick purchase window.

---
+++ AlbionDataAvalonia/Network/Handlers/AuctionGetLoadoutOffersResponseHandler.cs
@@ -0,0 +1,35 @@
+﻿using Albion.Network;
+using AlbionDataAvalonia.Network.Models;
+using AlbionDataAvalonia.Network.Responses;
+using AlbionDataAvalonia.Network.Services;
+using AlbionDataAvalonia.State;
+using System.Threading.Tasks;
+
+namespace AlbionDataAvalonia.Network.Handlers;
+
+public class AuctionGetLoadoutOffersResponseHandler : ResponsePacketHandler<AuctionGetLoadoutOffersResponse>
+{
+    private readonly Uploader uploader;
+    private readonly PlayerState playerState;
+    public AuctionGetLoadoutOffersResponseHandler(Uploader uploader, PlayerState playerState) : base((int)OperationCodes.AuctionGetLoadoutOffers)
+    {
+        this.uploader = uploader;
+        this.playerState = playerState;
+    }
+
+    protected override async Task OnActionAsync(AuctionGetLoadoutOffersResponse value)
+    {
+        if (!playerState.CheckOkToUpload()) return;
+
+        MarketUpload marketUpload = new MarketUpload();
+
+        value.marketOrders.ForEach(x => x.LocationId = playerState.Location.Id.ToString());
+        marketUpload.Orders.AddRange(value.marketOrders);
+
+        if (marketUpload.Orders.Count > 0)
+        {
+            uploader.EnqueueUpload(new Upload(marketUpload, null, null));
+        }
+        await Task.CompletedTask;
+    }
+}
