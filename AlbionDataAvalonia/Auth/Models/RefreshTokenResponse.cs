8704d4e5631c7899d41bf89ed7e92dcc7a573bb6
JP Codecraft <jpcode@craft.com>
2025-01-08 16:49:49 -0300

    login initial

---
+++ AlbionDataAvalonia/Auth/Models/RefreshTokenResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AlbionDataAvalonia.Auth.Models;
+
+public class RefreshTokenResponse
+{
+    [JsonPropertyName("expires_in")]
+    public string ExpiresIn { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; }
+
+    [JsonPropertyName("id_token")]
+    public string IdToken { get; set; }
+
+    [JsonPropertyName("user_id")]
+    public string UserId { get; set; }
+
+    [JsonPropertyName("project_id")]
+    public string ProjectId { get; set; }
+}
