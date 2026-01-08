e414050e220c0763824d81a063ae84a742a133ac
JP Codecraft <albion@gmail.com>
2026-01-08 19:56:10 -0300

    Add CharacterStatsEvent and CharacterStatsEventHandler classes

---
+++ AlbionDataAvalonia/Network/Events/CharacterStatsEvent.cs
@@ -0,0 +1,17 @@
+﻿using Albion.Network;
+using Serilog;
+using System.Collections.Generic;
+
+namespace AlbionDataAvalonia.Network.Events
+{
+    public class CharacterStatsEvent : BaseEvent
+    {
+        public Dictionary<byte, object> Parameters { get; }
+
+        public CharacterStatsEvent(Dictionary<byte, object> parameters) : base(parameters)
+        {
+            Log.Verbose("Got {PacketType} packet.", GetType());
+            Parameters = parameters;
+        }
+    }
+}
---
+++ AlbionDataAvalonia/Network/Events/Handlers/CharacterStatsEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Albion.Network;
+using AlbionDataAvalonia.Network.Events;
+using AlbionDataAvalonia.Shared;
+using System.Threading.Tasks;
+
+namespace AlbionDataAvalonia.Network.Handlers;
+
+public class CharacterStatsEventHandler : EventPacketHandler<CharacterStatsEvent>
+{
+    public CharacterStatsEventHandler() : base((int)EventCodes.CharacterStats)
+    {
+    }
+
+    protected override async Task OnActionAsync(CharacterStatsEvent value)
+    {
+        await Task.CompletedTask;
+    }
+}
